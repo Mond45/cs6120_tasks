e2f0bd38f2fa71b33ab72a93adadfbbb81194783
Mond45 <mondc45@gmail.com>
2025-09-15 01:34:55 -0400

    [task3] implement constant folding, add turnt tests

--- task3/src/bin/lvn.rs
+++ task3/src/bin/lvn.rs
@@ -1,14 +1,18 @@
+use std::env::args;
+
 use bril_rs::{load_program, output_program};
 use task3::{flatten, get_basic_blocks, lvn::lvn};
 
 fn main() {
     let mut program = load_program();
 
+    let constant_folding = args().any(|arg| arg == "-f");
+
     for function in program.functions.iter_mut() {
         let mut basic_blocks = get_basic_blocks(function);
 
         for block in basic_blocks.iter_mut() {
-            lvn(block);
+            lvn(block, constant_folding);
         }
 
         function.instrs = flatten(basic_blocks);
--- task3/src/lvn.rs
+++ task3/src/lvn.rs
@@ -3,12 +3,12 @@ use std::{
     sync::atomic::AtomicUsize,
 };
 
-use bril_rs::{Code, Instruction, Literal, Type, ValueOps};
+use bril_rs::{Code, ConstOps, Instruction, Literal, Type, ValueOps};
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Debug)]
 enum Value {
     Const(Type, Literal),
-    External,
+    External(String),
     ValueOp(ValueOps, Vec<usize>),
 }
 
@@ -23,7 +23,7 @@ fn can_reuse(op: ValueOps) -> bool {
 fn generate_var_name(original_name: &str) -> String {
     static COUNTER: AtomicUsize = AtomicUsize::new(0);
     format!(
-        "{}__{:0>9}",
+        "{}.{}",
         original_name,
         COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed)
     )
@@ -68,12 +68,66 @@ fn get_assignments(block: &[Code]) -> HashMap<String, Vec<usize>> {
     assignments
 }
 
-pub fn lvn(block: &mut Vec<Code>) {
+fn fold_constant(
+    op: ValueOps,
+    args: &[String],
+    var2idx: &HashMap<String, usize>,
+    table: &Vec<(Value, String)>,
+) -> Option<Value> {
+    use ValueOps::*;
+
+    let args_value: Vec<_> = args
+        .iter()
+        .map(|arg| &table[*var2idx.get(arg).unwrap()].0)
+        .collect();
+
+    match args_value.as_slice() {
+        [
+            Value::Const(Type::Int, Literal::Int(a)),
+            Value::Const(Type::Int, Literal::Int(b)),
+        ] => match op {
+            Add => Some(Value::Const(Type::Int, Literal::Int(a + b))),
+            Sub => Some(Value::Const(Type::Int, Literal::Int(a - b))),
+            Mul => Some(Value::Const(Type::Int, Literal::Int(a * b))),
+            Div if *b != 0 => Some(Value::Const(Type::Int, Literal::Int(a / b))),
+            Lt => Some(Value::Const(Type::Bool, Literal::Bool(a < b))),
+            Le => Some(Value::Const(Type::Bool, Literal::Bool(a <= b))),
+            Eq => Some(Value::Const(Type::Bool, Literal::Bool(a == b))),
+            Ge => Some(Value::Const(Type::Bool, Literal::Bool(a >= b))),
+            Gt => Some(Value::Const(Type::Bool, Literal::Bool(a > b))),
+            _ => None,
+        },
+        [
+            Value::Const(Type::Bool, Literal::Bool(a)),
+            Value::Const(Type::Bool, Literal::Bool(b)),
+        ] => match op {
+            And => Some(Value::Const(Type::Bool, Literal::Bool(*a && *b))),
+            Or => Some(Value::Const(Type::Bool, Literal::Bool(*a || *b))),
+            _ => None,
+        },
+        [Value::Const(Type::Bool, Literal::Bool(a)), _]
+        | [_, Value::Const(Type::Bool, Literal::Bool(a))] => match op {
+            And if !a => Some(Value::Const(Type::Bool, Literal::Bool(false))),
+            Or if *a => Some(Value::Const(Type::Bool, Literal::Bool(true))),
+            _ => None,
+        },
+        [Value::Const(Type::Bool, Literal::Bool(a))] if op == Not => {
+            Some(Value::Const(Type::Bool, Literal::Bool(!*a)))
+        }
+        [a, b] if a == b => match op {
+            Ge | Le | Eq => Some(Value::Const(Type::Bool, Literal::Bool(true))),
+            _ => None,
+        },
+        _ => None,
+    }
+}
+
+pub fn lvn(block: &mut Vec<Code>, constant_folding: bool) {
     let mut table: Vec<(Value, String)> = Vec::new();
     let mut var2idx: HashMap<String, usize> = HashMap::new();
 
     for var in get_external_vars(&block) {
-        table.push((Value::External, var.clone()));
+        table.push((Value::External(var.clone()), var.clone()));
         var2idx.insert(var, table.len() - 1);
     }
 
@@ -142,18 +196,39 @@ pub fn lvn(block: &mut Vec<Code>) {
                     op_type,
                     ..
                 } => {
-                    let instr_value = Value::ValueOp(
-                        op.clone(),
-                        args.iter()
-                            .map(|arg| var2idx.get(arg).unwrap().clone())
-                            .collect(),
-                    );
+                    let instr_value = if let Some(val) = fold_constant(*op, args, &var2idx, &table)
+                        && constant_folding
+                    {
+                        val
+                    } else {
+                        Value::ValueOp(
+                            op.clone(),
+                            args.iter()
+                                .map(|arg| var2idx.get(arg).unwrap().clone())
+                                .collect(),
+                        )
+                    };
 
-                    if let Some((idx_table, (_, var))) = table
+                    if let Value::Const(_, literal) = &instr_value {
+                        updates.push((
+                            idx_instr,
+                            Code::Instruction(Instruction::Constant {
+                                dest: dest.clone(),
+                                op: ConstOps::Const,
+                                pos: None,
+                                const_type: op_type.clone(),
+                                value: literal.clone(),
+                            }),
+                        ));
+
+                        table.push((instr_value, dest.clone()));
+
+                        var2idx.insert(dest.clone(), table.len() - 1);
+                    } else if let Some((idx_table, (_, var))) = table
                         .iter()
                         .enumerate()
                         .find(|(_, (val, _))| *val == instr_value)
-                        && can_reuse(*op)
+                        && (can_reuse(*op) || matches!(instr_value, Value::Const(..)))
                     {
                         updates.push((
                             idx_instr,
